6ce5506800a57c5fed5d7c6ecb35cd15d04086f9
novajade <zzongboy@naver.com>
2019-02-19 18:35:30 +0900

    response null Execption event

--- MainActivity.java
+++ MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.zzong.openapipractice;
 
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -10,6 +11,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.zzong.openapipractice.Retrofit.NaverInterface;
 import com.google.gson.Gson;
@@ -26,10 +28,11 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
-    public String Naver_id = "";
-    public String Naver_pw = "";
+    public String Naver_id = "02CeY0VDcCFFCgc6MTPq";
+    public String Naver_pw = "zJqe7__5Hb";
     public String keyword;
 
+    private View mainLayout;
     private ListView listView;
     private Button searchBtn;
     public String TAG = "OPEN API";
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mainLayout = findViewById(R.id.activity_main);
         listView = (ListView) findViewById(R.id.item_list);
         editText = (EditText) findViewById(R.id.search_bar);
         searchBtn = (Button) findViewById(R.id.search_button);
@@ -54,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
         editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if(i == EditorInfo.IME_ACTION_SEARCH) {
+                    if(i == EditorInfo.IME_ACTION_SEARCH) {
                     keyword = editText.getText().toString();
                     editText.onEditorAction(EditorInfo.IME_ACTION_DONE);//어플 시작에 키보드 appear 방지
                     Call<MovieData> call = naverInterface.getMovieData(
@@ -67,9 +71,18 @@ public class MainActivity extends AppCompatActivity {
                         public void onResponse(Call<MovieData> call, Response<MovieData> response) {
                             if (response.code() == 400) {
                                 Log.d(TAG, "WRONG QUERY");
+                                Snackbar.make(mainLayout.getRootView(), "검색창이 비어있습니다. 키워드를 입력해 주세요", Snackbar.LENGTH_LONG).show();
+                                return;
                             }
-                            List<items> movieitems = response.body().getItems();
-                            listView.setAdapter(new MovieAdapter(MainActivity.this, movieitems));
+                            else {
+                                try {
+                                    List<items> movieitems = response.body().getItems();
+                                    listView.setAdapter(new MovieAdapter(MainActivity.this, movieitems));
+                                }
+                                catch (Exception e){
+                                        Snackbar.make(mainLayout.getRootView(), "오류가 생겼습니다 다시 시도해보세요", Snackbar.LENGTH_LONG).show();
+                                }
+                            }
                         }
 
                         @Override
@@ -98,9 +111,20 @@ public class MainActivity extends AppCompatActivity {
                 call.enqueue(new Callback<MovieData>() {
                     @Override
                     public void onResponse(Call<MovieData> call, Response<MovieData> response) {
-                        if(response.code() == 400){ Log.d(TAG, "WRONG QUERY"); }
-                        List<items> movieitems = response.body().getItems();
-                        listView.setAdapter(new MovieAdapter(MainActivity.this, movieitems));
+                        if(response.code() == 400){
+                            Log.d(TAG, "WRONG QUERY");
+                            Snackbar.make(mainLayout.getRootView(), "검색창이 비어있습니다. 키워드를 입력해 주세요", Snackbar.LENGTH_LONG).show();
+                            return;
+                        }
+                        else {
+                            try {
+                                List<items> movieitems = response.body().getItems();
+                                listView.setAdapter(new MovieAdapter(MainActivity.this, movieitems));
+                            }
+                            catch (Exception e){
+                                Snackbar.make(mainLayout.getRootView(), "오류가 생겼습니다 다시 시도해보세요", Snackbar.LENGTH_LONG).show();
+                            }
+                        }
                     }
 
                     @Override
@@ -111,24 +135,5 @@ public class MainActivity extends AppCompatActivity {
                 });
             }
         });
-        /*
-        call.enqueue(new Callback<MovieData>() {
-            @Override
-            public void onResponse(Call<MovieData> call, Response<MovieData> response) {
-                if(response.code() == 400){
-                    Log.d(TAG, "Query Wrong");
-                }
-                else{
-                    Log.d(TAG, "DK What..");
-                }
-                MovieData movieDataList = response.body();
-                List<items> movieitems = response.body().getItems();
-
-                for(items itemscount : movieitems){
-                    Log.d(TAG, itemscount.getUserrating().toString());
-                }
-                listView.setAdapter(new MovieAdapter(MainActivity.this, movieitems));
-            }
-*/
     }
 }
